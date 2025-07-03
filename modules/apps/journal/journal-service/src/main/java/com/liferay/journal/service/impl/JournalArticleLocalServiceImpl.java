cddf6e8e76cd653e4ac5b749a69bdebd11fa7daa
Akhash R <akhash.ramprakash@liferay.com>
2025-07-03 14:13:37 +0530

    LPD-59259 Avoid copying model resources during revert; retain for copy operations

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -7591,9 +7591,11 @@ public class JournalArticleLocalServiceImpl
 
 		// Resources
 
-		_resourceLocalService.copyModelResources(
-			sourceArticle.getCompanyId(), JournalArticle.class.getName(),
-			sourceArticle.getResourcePrimKey(), resourcePrimKey);
+		if (newArticle) {
+			_resourceLocalService.copyModelResources(
+				sourceArticle.getCompanyId(), JournalArticle.class.getName(),
+				sourceArticle.getResourcePrimKey(), resourcePrimKey);
+		}
 
 		// Small image
 
