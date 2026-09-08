81d976854452e05a7ccdc118bca8ffc7823bfe03
Chaitanya Sammetla <chaitanya.sammetla@liferay.com>
2026-09-08 16:19:59 +0530

    LPD-104867 Apply the previewed item's context on a fragment config update
    UpdateConfigurationValuesMVCActionCommand always rendered a fragment
    configuration update with a fresh, empty DefaultFragmentRendererContext,
    so the previewed object entry (now sent by the client) had nowhere to go
    and data a fragment fetched via restClient, keyed on that entry, dropped
    out of the render. This reads the three new item parameters and applies
    them to the render context before calling the getFragmentEntryLinkJSONObject
    overload that accepts one, using the same InfoItemObjectProvider lookup,
    InfoItemReference, linked asset entry, and LayoutDisplayPageProvider swap
    that GetFragmentEntryLinksMVCResourceCommand already applies for the
    "Preview With" render path.

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateConfigurationValuesMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateConfigurationValuesMVCActionCommand.java
@@ -5,6 +5,10 @@
 
 package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.util.LinkedAssetEntryIdsUtil;
 import com.liferay.fragment.constants.FragmentEntryLinkConstants;
 import com.liferay.fragment.listener.FragmentEntryLinkListener;
 import com.liferay.fragment.listener.FragmentEntryLinkListenerRegistry;
@@ -12,26 +16,44 @@ import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.processor.DefaultFragmentEntryProcessorContext;
 import com.liferay.fragment.processor.FragmentEntryProcessorContext;
 import com.liferay.fragment.processor.FragmentEntryProcessorRegistry;
+import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
 import com.liferay.fragment.service.FragmentEntryLinkService;
 import com.liferay.fragment.util.configuration.FragmentConfigurationField;
 import com.liferay.fragment.util.configuration.FragmentEntryConfigurationParser;
+import com.liferay.info.constants.InfoDisplayWebKeys;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.ERCInfoItemIdentifier;
+import com.liferay.info.item.InfoItemIdentifier;
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.info.item.InfoItemServiceRegistry;
+import com.liferay.info.item.provider.InfoItemDetailsProvider;
+import com.liferay.info.item.provider.InfoItemObjectProvider;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
 import com.liferay.layout.content.page.editor.web.internal.manager.FragmentEntryLinkManager;
 import com.liferay.layout.content.page.editor.web.internal.util.layout.structure.LayoutStructureUtil;
+import com.liferay.layout.display.page.LayoutDisplayPageProvider;
+import com.liferay.layout.display.page.LayoutDisplayPageProviderRegistry;
+import com.liferay.layout.display.page.constants.LayoutDisplayPageWebKeys;
 import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.portlet.ActionRequest;
 import jakarta.portlet.ActionResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -99,6 +121,33 @@ public class UpdateConfigurationValuesMVCActionCommand
 						editableFragmentEntryProcessorJSONObject.get(
 							editableId));
 				}
+			}
+		}
+	}
+
+	private void _addLinkedAssetEntryId(
+		String className, long classPK, HttpServletRequest httpServletRequest) {
+
+		AssetRendererFactory<?> assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				className);
+
+		if (assetRendererFactory == null) {
+			return;
+		}
+
+		try {
+			AssetEntry assetEntry = assetRendererFactory.getAssetEntry(
+				className, classPK);
+
+			if (assetEntry != null) {
+				LinkedAssetEntryIdsUtil.addLinkedAssetEntryId(
+					httpServletRequest, assetEntry.getEntryId());
+			}
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
 			}
 		}
 	}
@@ -186,6 +235,12 @@ public class UpdateConfigurationValuesMVCActionCommand
 		String editableValues = ParamUtil.getString(
 			actionRequest, "editableValues");
 
+		String itemClassName = ParamUtil.getString(
+			actionRequest, "itemClassName");
+		long itemClassPK = ParamUtil.getLong(actionRequest, "itemClassPK");
+		String itemExternalReferenceCode = ParamUtil.getString(
+			actionRequest, "itemExternalReferenceCode");
+
 		FragmentEntryLink fragmentEntryLink =
 			_fragmentEntryLinkService.updateFragmentEntryLink(
 				fragmentEntryLinkId, editableValues);
@@ -211,15 +266,105 @@ public class UpdateConfigurationValuesMVCActionCommand
 				themeDisplay.getScopeGroupId(), themeDisplay.getPlid(),
 				fragmentEntryLink.getSegmentsExperienceId());
 
+		DefaultFragmentRendererContext defaultFragmentRendererContext =
+			new DefaultFragmentRendererContext(fragmentEntryLink);
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			actionRequest);
+
+		LayoutDisplayPageProvider<?> currentLayoutDisplayPageProvider =
+			(LayoutDisplayPageProvider<?>)httpServletRequest.getAttribute(
+				LayoutDisplayPageWebKeys.LAYOUT_DISPLAY_PAGE_PROVIDER);
+
+		if (Validator.isNotNull(itemClassName) &&
+			((itemClassPK > 0) ||
+			 Validator.isNotNull(itemExternalReferenceCode))) {
+
+			InfoItemIdentifier infoItemIdentifier = null;
+
+			if (itemClassPK > 0) {
+				infoItemIdentifier = new ClassPKInfoItemIdentifier(itemClassPK);
+			}
+			else {
+				infoItemIdentifier = new ERCInfoItemIdentifier(
+					itemExternalReferenceCode);
+			}
+
+			InfoItemObjectProvider<Object> infoItemObjectProvider =
+				_infoItemServiceRegistry.getFirstInfoItemService(
+					InfoItemObjectProvider.class, itemClassName,
+					infoItemIdentifier.getInfoItemServiceFilter());
+
+			if (infoItemObjectProvider != null) {
+				Object infoItemObject = infoItemObjectProvider.getInfoItem(
+					infoItemIdentifier);
+
+				defaultFragmentRendererContext.setContextInfoItemReference(
+					new InfoItemReference(itemClassName, infoItemIdentifier));
+
+				httpServletRequest.setAttribute(
+					InfoDisplayWebKeys.INFO_ITEM, infoItemObject);
+
+				InfoItemDetailsProvider infoItemDetailsProvider =
+					_infoItemServiceRegistry.getFirstInfoItemService(
+						InfoItemDetailsProvider.class, itemClassName);
+
+				if (infoItemDetailsProvider != null) {
+					httpServletRequest.setAttribute(
+						InfoDisplayWebKeys.INFO_ITEM_DETAILS,
+						infoItemDetailsProvider.getInfoItemDetails(
+							infoItemObject));
+				}
+
+				httpServletRequest.setAttribute(
+					InfoDisplayWebKeys.INFO_ITEM_REFERENCE,
+					new InfoItemReference(itemClassName, infoItemIdentifier));
+
+				_addLinkedAssetEntryId(
+					itemClassName, itemClassPK, httpServletRequest);
+			}
+
+			LayoutDisplayPageProvider<?> layoutDisplayPageProvider =
+				_layoutDisplayPageProviderRegistry.
+					getLayoutDisplayPageProviderByClassName(
+						_portal.getCompanyId(httpServletRequest),
+						itemClassName);
+
+			if (layoutDisplayPageProvider != null) {
+				httpServletRequest.setAttribute(
+					LayoutDisplayPageWebKeys.LAYOUT_DISPLAY_PAGE_PROVIDER,
+					layoutDisplayPageProvider);
+			}
+		}
+
+		JSONObject fragmentEntryLinkJSONObject;
+
+		try {
+			fragmentEntryLinkJSONObject =
+				_fragmentEntryLinkManager.getFragmentEntryLinkJSONObject(
+					defaultFragmentRendererContext, fragmentEntryLink,
+					httpServletRequest,
+					_portal.getHttpServletResponse(actionResponse),
+					layoutStructure);
+		}
+		finally {
+			httpServletRequest.removeAttribute(
+				InfoDisplayWebKeys.INFO_ITEM_REFERENCE);
+
+			httpServletRequest.setAttribute(
+				LayoutDisplayPageWebKeys.LAYOUT_DISPLAY_PAGE_PROVIDER,
+				currentLayoutDisplayPageProvider);
+		}
+
 		return JSONUtil.put(
-			"fragmentEntryLink",
-			_fragmentEntryLinkManager.getFragmentEntryLinkJSONObject(
-				fragmentEntryLink, _portal.getHttpServletRequest(actionRequest),
-				_portal.getHttpServletResponse(actionResponse), layoutStructure)
+			"fragmentEntryLink", fragmentEntryLinkJSONObject
 		).put(
 			"layoutData", layoutStructure.toJSONObject()
 		);
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpdateConfigurationValuesMVCActionCommand.class);
 
 	@Reference
 	private FragmentEntryConfigurationParser _fragmentEntryConfigurationParser;
@@ -238,10 +383,17 @@ public class UpdateConfigurationValuesMVCActionCommand
 	private FragmentEntryProcessorRegistry _fragmentEntryProcessorRegistry;
 
 	@Reference
+	private InfoItemServiceRegistry _infoItemServiceRegistry;
+
+	@Reference
 	private JSONFactory _jsonFactory;
 
 	@Reference
 	private Language _language;
+
+	@Reference
+	private LayoutDisplayPageProviderRegistry
+		_layoutDisplayPageProviderRegistry;
 
 	@Reference
 	private Portal _portal;
